bc23b1f689610b223aa9d6c01a05c4921d09367f
Artur Kudaev <artur.kudaev@ria.com>
2020-10-22 17:03:37 +0300

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -13,8 +13,9 @@ function App() {
 
   useEffect(() => {
     const parser = new Uaparser();
-    parser.setUA(navigator.userAgent);
+    parser.setUA(window.navigator.userAgent);
     const result = parser.getResult();
+    console.log(result)
     setInfo(result)
   }, [])
 
@@ -28,11 +29,14 @@ function App() {
           <Route exact={true} path='/global' component={Global}/>
           <Route exact={true} path='/countries' component={Countries}/>
           <Route exact={true} path='/' component={Start}/>
-          {
-            info ?
-                <span className='device'>{info.device.vendor} {info.device.model}</span>
-                : null
-          }
+          <div className='device'>
+            device:
+            {
+              info ?
+                  <span> {info.device.vendor} {info.device.model}</span>
+                  : null
+            }
+          </div>
         </main>
         <footer>
           <Nav/>
