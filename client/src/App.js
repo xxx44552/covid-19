665fae2f4ae41561bebd199a100c39dc4c8fd5c8
Artur Kudaev <artur.kudaev@ria.com>
2020-10-22 16:09:32 +0300

    test device

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Global from "./global";
 import Countries from "./countries";
 import Nav from "./nav";
 import {BrowserRouter} from "react-router-dom";
 import {Route} from "react-router";
 import Start from "./start";
+import Uaparser from 'ua-parser-js'
 
 function App() {
+
+  const parser = new Uaparser();
+  parser.setUA(navigator.userAgent);
+  const result = parser.getResult();
+
+  const [info] = useState(result);
 
   return (
     <BrowserRouter>
@@ -18,6 +25,11 @@ function App() {
           <Route exact={true} path='/global' component={Global}/>
           <Route exact={true} path='/countries' component={Countries}/>
           <Route exact={true} path='/' component={Start}/>
+          {
+            info ?
+                <span className='device'>{info.device.vendor} {info.device.model}</span>
+                : null
+          }
         </main>
         <footer>
           <Nav/>
